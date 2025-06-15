eb6c2921efc7d8f3ca77b9e10d4e36cd2556da87
Prefixmodz <166675297+Prefixmodz@users.noreply.github.com>
2025-06-15 19:02:45 +0100

    fix fix fix

--- T7_luaLoader/prx.cpp
+++ T7_luaLoader/prx.cpp
@@ -4,22 +4,29 @@
 SYS_MODULE_INFO(T7_luaLoader, 0, 1, 1);
 SYS_MODULE_START(module_start);
 SYS_MODULE_STOP(module_stop);
-
 SYS_LIB_DECLARE_WITH_STUB(LIBNAME, SYS_LIB_AUTO_EXPORT, STUBNAME);
 SYS_LIB_EXPORT(T7_luaLoader_export_function, LIBNAME);
 
 void Run_raw_lua(std::string data)
 {
-    if (!luaVM || data.empty())
+    lua_Reader readerData;
+
+    readerData.buffer = data.c_str();
+    readerData.length = data.size();
+
+    if (!luaVM || !readerData.buffer || readerData.length == 0)
         return;
 
     luaVM->m_global->m_bytecodeSharingMode = HKS_BYTECODE_SHARING_ON;
 
-    hksi_hksL_loadbuffer(luaVM, &luaVM->m_global->m_compilerSettings, data.c_str(), data.size(), data.c_str());
+   // hksi_hksL_loadbuffer(luaVM, &luaVM->m_global->m_compilerSettings, data.c_str(), data.size(), data.c_str());
+
+    if (!hksi_hks_load(luaVM, &luaVM->m_global->m_compilerSettings, (void*)hks_bufferReader_a, &readerData, 0, 0, readerData.buffer))
+        hks_vm_call_internal(luaVM, 0, 0, 0);
 
     luaVM->m_global->m_bytecodeSharingMode = HKS_BYTECODE_SHARING_SECURE;
 
-    hksi_lua_pcall(luaVM, 0, 0, 0);
+   // hksi_lua_pcall(luaVM, 0, 0, 0);
 }
 
 extern "C"
@@ -31,7 +38,7 @@ extern "C"
 
     int module_start(void)
     {
-        printf("[T7_luaLoader By PrefixModz]: Loaded!\n");
+        printf("[T7_luaLoader]: Loaded!\n");
 
         Run_raw_lua("Dvar.ui_lobbyDebugVis:set(1)");
         
@@ -40,7 +47,7 @@ extern "C"
 
     int module_stop(void)
     {
-        printf("[T7_luaLoader By PrefixModz]: Unloaded!\n");
+        printf("[T7_luaLoader]: Unloaded!\n");
 
         Run_raw_lua("Dvar.ui_lobbyDebugVis:set(0)");
 
--- T7_luaLoader/symbols.h
+++ T7_luaLoader/symbols.h
@@ -1,6 +1,6 @@
 #pragma once
 
-
+static symbol<int(lua_state* s, const HksCompilerSettings* options, void* reader, void* readerData, void* debugReader, void* debugReaderData, const char* chunkName)>hksi_hks_load{ hksi_hks_load_a };
 static symbol<void(lua_state* s, const HksCompilerSettings* options, const char* buff, size_t sz, const char* name)>hksi_hksL_loadbuffer{ hksi_hksL_loadbuffer_a };
 static symbol<int(lua_state* s, int nargs, int nresults, int errfunc)>hksi_lua_pcall{ hksi_lua_pcall_a };
 static symbol<void(lua_state* s, uint32_t nArgs, uint32_t nResults, uint32_t pc)>hks_vm_call_internal{ hks_vm_call_internal_a };
